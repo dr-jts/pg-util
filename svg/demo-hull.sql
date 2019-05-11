fdddaf46c768661636135d49538153ae0504cddb
Martin Davis <mtnclimb@gmail.com>
2019-05-11 11:34:50 -0700

    Add SVG hull demo

---
+++ svg/demo-hull.sql
@@ -0,0 +1,31 @@
+WITH input AS (
+  SELECT 'MULTIPOINT ((158 60), (105 64), (109 196), (87 172), (75 76), (100 111), (99 33), (150 78), (95 91), (81 209), (78 153), (172 92), (109 159), (179 99), (80 121), (80 128), (52 154), (181 104), (166 81), (71 61), (131 60), (101 82), (126 48), (173 180), (109 171), (169 86), (135 170), (55 62), (103 197), (135 187))'::geometry geom
+),
+convex AS (
+  SELECT ST_ConvexHull( geom ) AS geom FROM input
+),
+concave AS (
+  SELECT ST_ConcaveHull( geom, 0.99 ) AS geom FROM input
+),
+shapes AS (
+  SELECT geom, svgShape( geom,
+    style => svgStyle('stroke', '#aa00aa',
+        'stroke-width', 1::text,
+        'fill', '#ff80ff',
+        'stroke-linejoin', 'round' ) )
+    svg FROM convex
+  UNION ALL
+  SELECT geom, svgShape( geom,
+    style => svgStyle('stroke', '#0000ff',
+        'stroke-width', 1::text,
+        'fill', '#a0a0ff',
+        'stroke-linejoin', 'round' ) )
+    svg FROM concave
+  UNION ALL
+  SELECT geom, svgShape( geom, radius=>2,
+    style => svgStyle( 'fill', '#ff0000'  ) )
+    svg FROM input
+)
+SELECT svgDoc( array_agg( svg ),
+  ST_Expand( ST_Extent(geom), 5)
+  ) AS svg FROM shapes;
