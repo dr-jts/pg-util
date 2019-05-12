f67e7345e83bd45d6903d701b16dc54574d67f26
Martin Davis <mtnclimb@gmail.com>
2019-05-12 11:01:55 -0700

    formatting

--- svg/demo/demo-hilbert.sql
+++ svg/demo/demo-hilbert.sql
@@ -3,7 +3,6 @@
 --
 -- Parameter: iteration
 -- iteration dependent values in SVG:
---   viewBox
 --   id divisor hsl function
 -- ---------------------------------
 
@@ -18,19 +17,21 @@ path AS (
   FROM (SELECT state FROM lsystem ORDER BY iteration DESC LIMIT 1) st
 ),
 pts AS (
-  SELECT moves AS moves, 1 AS index, ' ' AS dir, 0 AS xp, 0 AS yp, 0 AS x, 0 AS y, 1 AS dx, 0 AS dy, 0 AS len  from path
+  SELECT moves AS moves, 1 AS index, ' ' AS dir,
+      0 AS xp, 0 AS yp, 0 AS x, 0 AS y, 1 AS dx, 0 AS dy, 0 AS len
+    FROM path
   UNION ALL
   SELECT moves, index+1 AS index, substr(moves, index, 1) AS dir,
       x AS xp, y AS yp,
-      x + len*dx AS x,
-      y + len*dy AS y,
+      x + len*dx AS x, y + len*dy AS y,
       CASE substr(moves, index, 1) WHEN '-' THEN -dy WHEN '+' THEN  dy ELSE dx END AS dx,
       CASE substr(moves, index, 1) WHEN '-' THEN  dx WHEN '+' THEN -dx ELSE dy END AS dy,
       CASE substr(moves, index, 1) WHEN 'F' THEN 1 ELSE 0 END AS len
     FROM pts WHERE index <= length(moves)
 ),
 hilbert AS (
-  SELECT row_number() over() as id, ST_MakeLine( ST_MakePoint(xp, yp), ST_MakePoint(x, y)) geom FROM pts WHERE xp <> x OR yp <> y
+  SELECT row_number() over() as id, ST_MakeLine( ST_MakePoint(xp, yp), ST_MakePoint(x, y)) geom
+  FROM pts WHERE xp <> x OR yp <> y
 ),
 svg AS (
     SELECT geom, svgShape( geom,
