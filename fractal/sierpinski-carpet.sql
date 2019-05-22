0900f16096cb94afdc1c8341a8f1ab9199b32c28
Martin Davis <mtnclimb@gmail.com>
2019-05-22 15:00:29 -0700

    SQL formatting

--- fractal/sierpinski-carpet.sql
+++ fractal/sierpinski-carpet.sql
@@ -3,24 +3,26 @@
 -- Parameter: level
 -- ---------------------------------
 
-WITH RECURSIVE centres AS (
-  SELECT 0.5 as x, 0.5 as y, 1.0/3.0 AS size, 1 as level
-  UNION ALL 
-  SELECT x + dx * size, y + dy * size, size/3.0, level
+WITH RECURSIVE
+centres( level, x, y, size ) AS (
+  SELECT 1 AS level, 0.5 as x, 0.5 as y, 1.0/3.0 AS size
+  UNION ALL
+  SELECT level, x + dx * size, y + dy * size, size/3.0
     FROM (
-      SELECT x, y, dx, dy, 1.0/(3.0 ^ level) AS size, level+1 as level 
+      SELECT level + 1 AS level, x, y, dx, dy, 1.0/(3.0 ^ level) AS size
       FROM generate_series(-1, 1) AS xoff(dx)
         CROSS JOIN generate_series(-1, 1) AS yoff(dy)
         CROSS JOIN centres
-      WHERE not( dx = 0 AND dy = 0) AND level < 3
+      WHERE NOT ( dx = 0 AND dy = 0) AND level < 3
     ) dd
 ),
-holes as (
+holes( hole ) AS (
   SELECT ST_MakeEnvelope(x - size/2, y - size/2, x + size/2, y + size/2) AS hole
   FROM centres
 ),
-sierpinski_carpet AS (
-  SELECT ST_MakePolygon( ST_ExteriorRing(ST_MakeEnvelope(0,0,1,1)), 
-    array_agg( ST_ExteriorRing(hole)) ) AS geom from holes
+sierpinski_carpet( geom ) AS (
+  SELECT ST_MakePolygon( ST_ExteriorRing(ST_MakeEnvelope(0,0,1,1)),
+    array_agg( ST_ExteriorRing(hole)) ) AS geom
+  FROM holes
 )
-select ST_AsGeoJSON( ST_Translate( ST_Scale(geom, 10, 10 ), 0, 23), 2 ) from sierpinski_carpet;
+SELECT ST_AsGeoJSON( ST_Translate( ST_Scale(geom, 10, 10 ), 0, 23), 2 ) FROM sierpinski_carpet;
